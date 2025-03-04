e9593175f8f7ff62a9eac09ad5e079286484d29d
CodePawsDev <isripen1991@outlook.com>
2025-03-04 18:06:12 +0700

    feat: add initial exercises

---
+++ th/ex1-1.js
@@ -0,0 +1,17 @@
+/* 
+- เราอยากพัฒนาโปรแกรมบวกลบคูณหารเลข ทุกครั้งจะรับ Parameter 3 ตัวคือ
+    - เลขตัวที่ 1
+    - เลขตัวที่ 2
+    - กระบวนการทางคณิตศาสตร์
+- ให้เขียน function ที่ทำงานตามที่โจทย์กำหนด
+*/
+
+// Start Coding Here
+
+
+// เมื่อ function นี้ทำงานจะเห็นผลลัพธ์ดังนี้
+console.log(calculate(10, 5, add)); // 15
+console.log(calculate(10, 5, subtract)); // 5
+console.log(calculate(10, 5, multiply)); // 50
+console.log(calculate(10, 5, divide)); // 2
+console.log(calculate(10, 0, divide)); // "Cannot divide by zero"
---
+++ th/ex2-2.js
@@ -0,0 +1,14 @@
+/* 
+1. Callback & Higher-Order Function มีข้อดีอย่างไร
+Answer:
+
+
+2. จากการลองใช้ Callback Function ในระบบเกมและระบบแจ้งเตือน คุณคิดว่าโค้ดอ่านง่ายขึ้นหรือไม่ เพราะอะไร 
+Answer:
+
+
+3. หากต้องสอนเพื่อนให้เข้าใจ allback & Higher-Order Function ใน 5 นาที จะอธิบายอย่างไร 
+Answer:  
+
+
+*/
